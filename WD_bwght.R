b620d8131b5e6dbf7e59df68d51e19bd1bd10b78
Paolo Imori <101586885+imoripaolo@users.noreply.github.com>
2022-05-30 21:11:54 -0300

    Rename bwght.R to WD_bwght.R

--- WD_bwght.R
+++ WD_bwght.R
@@ -1,10 +1,10 @@
 # Base: Examples and Exercises by Wooldridge, with base BWGHT.
 # Download it at Wooldridge's site.
-# Base: Exemplos e Exercícios de Woolrdrige, com base BWGHT.
-# Faça download no site do Wooldridge.
+# Base: Exemplos e ExercÃ­cios de Woolrdrige, com base BWGHT.
+# FaÃ§a download no site do Wooldridge.
 
 # Let's set a working directory and load packages
-# Vamos definir uma área de trabalho e carregar pacotes
+# Vamos definir uma Ã¡rea de trabalho e carregar pacotes
 setwd("~/R/Woodridge")
 library(readxl)
 
@@ -12,7 +12,7 @@ library(readxl)
 BWGHT <- read_excel("bwght.xls", col_names = FALSE)
 bwght <- as.data.frame(BWGHT)
 
-# Creating Variables / Criando Variáveis:
+# Creating Variables / Criando VariÃ¡veis:
 rendfam <- as.numeric(bwght[,1])
 pesonas <- as.numeric(bwght[,4])
 educp <- as.numeric(bwght[,5])
@@ -20,7 +20,7 @@ educm <- as.numeric(bwght[,6])
 ordnas <- as.numeric(bwght[,7])
 cigs <- as.numeric(bwght[,10])
 
-# Regression for exercise 2.11 / Regressão para exercício 2.11
+# Regression for exercise 2.11 / RegressÃ£o para exercÃ­cio 2.11
 reg2_11 <- lm(pesonas ~ cigs)
 summary(reg2_11)
 plot(cigs,pesonas)
@@ -32,6 +32,6 @@ beta0 + beta1*0 # (i)
 beta0 + beta1*20 # (i)
 (125 - beta0)/beta1 # (iii)
 
-# Regression for example 4.9 / Regressão para exemplo 4.9
+# Regression for example 4.9 / RegressÃ£o para exemplo 4.9
 reg4_9 <- lm(pesonas ~ cigs + ordnas + rendfam + educm + educp)
 summary(reg4_9)
