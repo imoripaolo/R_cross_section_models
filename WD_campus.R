1eaba1e81e834cd32152034da8ce9540ee90e438
Paolo Imori <101586885+imoripaolo@users.noreply.github.com>
2022-05-30 21:12:25 -0300

    Rename campus.R to WD_campus.R

--- WD_campus.R
+++ WD_campus.R
@@ -1,10 +1,10 @@
 # Base: Examples and Exercises by Wooldridge, with base CAMPUS.
 # Download it at Wooldridge's site.
-# Base: Exemplos e Exercícios de Woolrdrige, com base CAMPUS.
-# Faça download no site do Wooldridge.
+# Base: Exemplos e ExercÃ­cios de Woolrdrige, com base CAMPUS.
+# FaÃ§a download no site do Wooldridge.
 
 # Let's set a working directory and load packages
-# Vamos definir uma área de trabalho e carregar pacotes
+# Vamos definir uma Ã¡rea de trabalho e carregar pacotes
 setwd("~/R/Woodridge")
 library(readxl)
 
@@ -12,11 +12,11 @@ library(readxl)
 CAMPUS <- read_excel("campus.xls", col_names = FALSE)
 campus <- as.data.frame(CAMPUS)
 
-# Creating Variables / Criando Variáveis:
+# Creating Variables / Criando VariÃ¡veis:
 logcrime <- campus[,5]
 logmatricl <- campus[,6]
 
-# Regression for example 4.4 / Regressão para exemplo 4.4
+# Regression for example 4.4 / RegressÃ£o para exemplo 4.4
 reg4_4 <- lm(logcrime ~ logmatricl)
 summary(reg4_4)
 
